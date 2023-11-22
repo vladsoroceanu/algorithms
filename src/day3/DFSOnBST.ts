ec92c585d5d34e11dc1e0237523761274659d83d
Vlad Soroceanu <vlad@fabrikant.tech>
2023-11-22 17:04:07 +0000

    implemented depth-first search on a binary tree

--- src/day3/DFSOnBST.ts
+++ src/day3/DFSOnBST.ts
@@ -1,3 +1,19 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function search(crr: BinaryNode<number> | null, needle: number): boolean {
+    if (!crr) {
+        return false;
+    }
 
+    if (crr.value === needle) {
+        return true;
+    }
+
+    if (needle > crr.value) {
+        return search(crr.right, needle);
+    }
+
+    return search(crr.left, needle);
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return search(head, needle);
 }
